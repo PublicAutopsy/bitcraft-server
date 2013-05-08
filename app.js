999fd282903f2a26021fcda300c9a2239b13a40c
Cody <byrnes.cody@gmail.com>
2013-05-07 22:18:29 -0700

    getting Balance

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/api/getBalance', routes.getBalance);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,16 @@ var client = new bitcoin.Client('localhost', 3000, 'bitcoinrpc', 'C7rJWWWZWUjzZa
 
 exports.index = function(req, res){
     console.log(client);
+    res.end();
 };
+
+exports.makeWallet = function(req, res){
+
+}
+
+exports.getBalance = function(req, res){
+    client.getBalance("*", 0, function(err, balance){
+        console.log("Balance: "+balance);
+        res.end();
+    })
+}
